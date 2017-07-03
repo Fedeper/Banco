4c4e151ef2b71644a9f8e234cbccaca99e2f0fcc
Stefania <stefaniamichellepaz@gmail.com>
2017-07-03 00:53:18 -0300

    modulo personas

---
+++ personas.py
@@ -0,0 +1,10 @@
+class Persona():
+    def __init__(self,nombre,apellido,domicilio,telefono,f_Nac,dni,genero):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.domicilio=domicilio
+        self.telefono=telefono
+        self.f_Nac=f_Nac
+        self.dni=dni
+        self.genero=genero
+        
